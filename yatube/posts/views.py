8b999c41b47c7f41541909942845af6e1c615fbb
Денис Филлер <denis.filler@yandex.ru>
2023-03-12 20:48:58 +0300

    fixed comment

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-# Create your views here.
-
 
 def index(request):
     """Возвращает отображение главной страницы"""
